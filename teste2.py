d3b4f3c8d3ec9eaaae2bbd2fd7f54401cc2f23ac
marcos <marcos.monteirojr@gmail.com>
2017-09-12 09:58:20 -0300

    trabalhos

--- teste2.py
+++ teste2.py
@@ -1,108 +1,50 @@
-import numpy
+"""
+Testing for the base module (sklearn.ensemble.base).
+"""
 
-from matplotlib import pyplot as plot
-from matplotlib.backends.backend_pdf import PdfPages
-nome_base="Wine"
+# Authors: Gilles Louppe
+# License: BSD 3 clause
+
+from numpy.testing import assert_equal
+from nose.tools import assert_true
+
+from sklearn.utils.testing import assert_raise_message
+from sklearn.datasets import load_iris
+from sklearn.ensemble import BaggingClassifier
+from sklearn.linear_model import Perceptron
 
 
-def gera_grafico():
-    pdf_pages = PdfPages('histograms.pdf')
+def test_base():
+    # Check BaseEnsemble methods.
+    ensemble = BaggingClassifier(base_estimator=Perceptron(), n_estimators=3)
 
-    # Generate the pages
-    nb_plots = 14
+    iris = load_iris()
+    ensemble.fit(iris.data, iris.target)
+    ensemble.estimators_ = []  # empty the list and create estimators manually
 
-    nb_plots_per_page = 5
-    nb_pages = int(numpy.ceil(nb_plots / float(nb_plots_per_page)))
-    grid_size = (nb_plots_per_page, 2)
+    ensemble._make_estimator()
+    ensemble._make_estimator()
+    ensemble._make_estimator()
+    ensemble._make_estimator(append=False)
 
-    data1 = []
-    data2 = []
-    data5 = []
-    data6 = []
-    nome_base = 'Wine'
-    j = 0
-    r=0
-    for i in range(1, 15):
-        data3 = []
-        data4 = []
-        data1 = []
-        data2 = []
-        data5 = []
-        data6 = []
-        # arq1 = open('/home/marcos/Documents/Tese/ComplexidadeDist/' + nome_base + str(i) + '/Wine_medias.txt', 'r')
-        arq2 = open('/home/marcos/Documents/Tese/ComplexidadeDist/'+nome_base+'/' + nome_base + str(i) + '/'+nome_base+'_medias.txt', 'r')
-        arq1 = open('/home/marcos/Documents/Tese/ComplexidadeBags/'+nome_base+'/' + nome_base + str(i)+'/' + nome_base + '_medias.txt', 'r')
-        arq3 = open('/home/marcos/Documents/Tese/ComplexidadeAG/'+nome_base+'/' + nome_base + '1/' + nome_base + '_medias.txt', 'r')
-        for k in arq1:
-            b = k.replace(', ', ' ')
-            b=b.split(' ')
-            if b[0] == 'F1':
-                None
-            else:
-                #print(b[1])
-                data1.append(b[0])
-                data2.append(b[1])
-                data1= [float(w) for w in data1]
-                data2= [float(w) for w in data2]
-            #print (data1)
-            #data1 = (data1[1:])
-        for k in arq2:
-            b = k.replace(', ', ' ')
-            b = b.split(' ')
-            if b[0] == 'F1':
-                None
-            else:
-                #print(b[1])
-                data3.append(b[0])
-                data4.append(b[1])
-                data3 = [float(w) for w in data3]
-                data4 = [float(w) for w in data4]
-        for k in arq3:
-            b = k.replace(', ', ' ')
-            b = b.split(' ')
-            if b[0] == 'F1':
-                None
-            else:
-                #print(b[1])
-                data5.append(b[0])
-                data6.append(b[1])
-                data5 = [float(w) for w in data5]
-                data6 = [float(w) for w in data6]
-        #print(((data5)))
+    assert_equal(3, len(ensemble))
+    assert_equal(3, len(ensemble.estimators_))
 
-        if (r == 2):
-            r = 0
-        if j % nb_plots_per_page == 0:
-            fig = plot.figure(figsize=(8, 15), dpi=50)
-        print(j)
-        # Plot stuffs !
-        plot.subplot2grid(grid_size, (j % nb_plots_per_page, r))
-        plot.title(nome_base+str(i))
-        bag=plot.scatter(x=data1,y=data2,alpha=0.6)
-        dist=plot.scatter(x=data3, y=data4,alpha=0.6)
-        ag=plot.scatter(x=data5, y=data6, alpha=0.6)
-        plot.xlabel("N2")
-        plot.ylabel("F1")
-        plot.legend([bag, dist, ag], ["Bag", "Dist", "AG"])
-        # Close the page if needed
-        if (j + 1) % nb_plots_per_page == 0 or (j + 1) == nb_plots:
-            plot.tight_layout()
-            pdf_pages.savefig(fig)
-        j=j+1
-        r=r+1
+    assert_true(isinstance(ensemble[0], Perceptron))
 
-    pdf_pages.close()
+
+def test_base_zero_n_estimators():
+    # Check that instantiating a BaseEnsemble with n_estimators<=0 raises
+    # a ValueError.
+    ensemble = BaggingClassifier(base_estimator=Perceptron(), n_estimators=0)
+    iris = load_iris()
+    assert_raise_message(ValueError,
+                         "n_estimators must be greater than zero, got 0.",
+                          ensemble.fit, iris.data, iris.target)
 
 def main():
-    nome_base='Wine'
-    #
-    # for i in range(1,21):
-    # #arq1 = open('/home/marcos/Documents/Tese/ComplexidadeDist/' + nome_base + str(i) + '/Wine_medias.txt', 'r')
-    #     arq2 = open('/home/marcos/Documents/Tese/ComplexidadeDist/' + nome_base + str(i) + '/Wine_medias.txt', 'r')
-    #     arq1 = open('/home/marcos/Documents/Tese/ComplexidadeBags/' + nome_base + '/' + nome_base + '_medias.txt', 'r')
-    #     arq3 = open('/home/marcos/Documents/Tese/ComplexidadeAG/' + nome_base + '/' + nome_base + '_medias.txt', 'r')
-
-    gera_grafico()
+  test_base()
+  test_base_zero_n_estimators()
 
 
 
--- trab/Trab GA.py
+++ trab/Trab GA.py
@@ -1,6 +1,7 @@
-import arff
-from sklearn.model_selection import train_test_split, cross_val_score, ShuffleSplit, cross_val_predict, StratifiedShuffleSplit, StratifiedKFold
-
+import arff, numpy, random, os
+from sklearn.model_selection import train_test_split
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.externals import joblib
 
 def cria_dataset(dataset):
     lables =[]
@@ -14,10 +15,28 @@ def cria_dataset(dataset):
     X_test, X_val, y_test, y_val= train_test_split(X_x, y_y, test_size=0.5, random_state=None, stratify=y_y)#divide a base entre teste e validacao
     return X_train, X_test, X_val, y_train, y_test, y_val # X_ -> bases sem lables, y_-> lables
 
+def cria_classificadores(X_train, y_train, repeticoes):
+    #bags=list()
+
+    tree = DecisionTreeClassifier()
+    for i in range(repeticoes):
+        r = random.seed()
+        X_bag, X_test, y_bag, y_test = train_test_split(X_train, y_train, test_size=0.5, random_state=r, stratify=y_train)  # divide o treino para criar os bags
+        tree.fit(X_bag,y_bag)
+        joblib.dump(tree, "clf/TreeClas"+str(i)+".pkl")#salva o classificador
+
+def acuracia(classificador, X_test, y_test):#mede a acuracia do classificador
+    print(classificador.score(X_test,y_test))
+
+
 def main():
     dataset = arff.load(open('letter.arff'))
     X_train, X_test, X_val, y_train, y_test, y_val =  cria_dataset(dataset)
-    print (X_train)
-    print (y_train)
+    if (os.path.exists("clf/TreeClas99.pkl") == False):
+        cria_classificadores(X_train,y_train,100)
+    else:
+        for i in range(100):
+            classificador=joblib.load('clf/TreeClas'+str(i)+'.pkl')
+            acuracia(classificador,X_test,y_test )
 if __name__ == '__main__':
         main()
